e81eb57b174aabde4c36352f0732e82c33f01afd
Valscr <95771970+Valscr@users.noreply.github.com>
2023-01-22 16:18:56 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: valentin <valentin@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/01/22 12:15:26 by valentin          #+#    #+#              #
+#    Updated: 2023/01/22 12:15:29 by valentin         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SOURCES = server.c client.c
+OBJECTS = $(SOURCES:.c=.o)
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+all: server client
+
+bonus: server client
+
+server: server.o libft
+	$(CC) -o $@ $< -Llibft -lft
+
+client: client.o libft
+	$(CC) -o $@ $< -Llibft -lft
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $?
+
+libft:
+	make -C libft
+
+clean:
+	rm -f $(OBJECTS)
+	make -C libft clean
+	
+fclean: clean
+	rm -f server client libft/libft.a
+
+re: fclean all
+
+.PHONY: all bonus libft clean fclean re
---
+++ server.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 12:14:37 by valentin          #+#    #+#             */
+/*   Updated: 2023/01/22 15:44:32 by valentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+char	get_bit(unsigned char c, int i)
+{
+	int	j;
+	int	k;
+
+	j = 1;
+	k = 0;
+	while (k < i)
+	{
+			j *= 2;
+			k++;
+	}
+	c += j;
+	return (c);
+}
+
+static void	action(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 0;
+	static pid_t			client_pid = 0;
+	static unsigned char	c = 0;
+
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	if (sig == SIGUSR2)
+	c = get_bit(c, i);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		kill(client_pid, SIGUSR1);
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	s_sigaction;
+
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+	while (1)
+		pause();
+	return (0);
+}
